4fb49c7f5d386ea8a633604e56eeb875cb02ea6d
hadong <dhso@163.com>
2016-11-03 20:12:37 +0800

    init

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -3,7 +3,7 @@ $.parser.onComplete = function() {
 	$(".welcome-loading").fadeOut("slow", function() {
 		$(this).remove();
 	});
-}
+};
 var menuDate = [ {
 	'id' : 1,
 	'name' : '仓库信息',
@@ -64,20 +64,11 @@ var menuDate = [ {
 } ];
 
 $(document).ready(function() {
-	initMainMenu(menuDate);
-	mainMenuClickListener();
 	initSubMenu(menuDate);
 	subMenuClickListener();
+	hovertip($('.main-menu li').has('.icon-bell'), '消息提醒', 1);
+	hovertip($('.main-menu li').has('.icon-off'), '退出登录', 1);
 });
-function initMainMenu(data) {
-	laytpl($('#main_menu_tpl').html()).render(data, function(html) {
-		$('.main-menu').html(html);
-	});
-}
-function mainMenuClickListener() {
-	$('.main-menu li').live('click', function() {
-	});
-}
 function initSubMenu(data) {
 	$.each(data, function(index, item) {
 		laytpl($('#sub_menu_tpl').html()).render(item.sub, function(html) {
@@ -93,44 +84,57 @@ function subMenuClickListener() {
 	$('.sub-menu li').live(
 			'click',
 			function() {
-				tab.create($('.lay-tab'), $(this).attr('data-id'), $(this)
+				$tab.create($('.lay-tab'), $(this).attr('data-id'), $(this)
 						.find('cite').text(), $(this).attr('data-href'));
 				$('.sub-menu li').removeClass('active');
 				$(this).addClass('active');
 			});
 }
-var tab = {
+// tip
+function hovertip(dom, text, direction, color) {
+	dom.mouseover(function() {
+		$tip(text, this, direction, color);
+	});
+}
+function $tip(text, dom, direction, color) {
+	layer.tips(text, dom, {
+		tips : [ direction ? direction : 3, color ? color : '#393D49' ],
+		time : 1000
+	});
+}
+
+// 标签页
+var $tab = {
 	create : function(tabDom, id, title, url) {
 		if (tabDom.tabs('exists', title)) {
 			tabDom.tabs('select', title);
 			return;
 		}
-		progress.show('加载中', '正在加载中，请耐心等待...');
+		$progress.show('加载中', '正在加载中，请耐心等待...');
 		tabDom.tabs('add', {
 			id : 'tab_' + id,
 			title : title,
 			href : url,
 			closable : true,
-			cls : 'content-tab-header',
-			bodyCls : 'content-tab-content',
 			extractor : function(data) {
 				var res = JSON.parse(data);
 				consoloe.log(res);
 			},
 			onLoad : function() {
-				progress.close();
+				$progress.close();
 			}
 		});
 	}
 };
-var progress = {
+// loading进度条
+var $progress = {
 	show : function(title, text) {
 		parent.$.messager.progress({
 			title : title,
 			text : text
 		});
 		setTimeout(function() {
-			progress.close();
+			$progress.close();
 		}, timeout * 1000);
 	},
 	close : function() {
@@ -138,9 +142,10 @@ var progress = {
 	}
 };
 // IE8 console.log() 未定义错误
-window.console = window.console || (function(){ 
-	var c = {};
-	c.log = c.warn = c.debug = c.info = c.error = c.time = c.dir = c.profile 
-	= c.clear = c.exception = c.trace = c.assert = function(){}; 
-	return c; 
-})();
+window.console = window.console
+		|| (function() {
+			var c = {};
+			c.log = c.warn = c.debug = c.info = c.error = c.time = c.dir = c.profile = c.clear = c.exception = c.trace = c.assert = function() {
+			};
+			return c;
+		})();
